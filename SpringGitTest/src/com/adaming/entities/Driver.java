6e3de052bd78ddbf8831d6c824ab09cbd35bd89b
inti0298 <inti0298@DESKTOP-66N0E79>
2016-06-24 14:14:01 +0200

    v

--- SpringGitTest/src/com/adaming/entities/Driver.java
+++ SpringGitTest/src/com/adaming/entities/Driver.java
@@ -1,15 +1,16 @@
 package com.adaming.entities;
 
+
+
 public class Driver {
 
+	public static void maPremiereMethod(){
+		System.out.println();
+	}
 	public static void main(String[] args) {
 		
-		
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-
+		maPremiereMethod();
+	
 	}
 
 }
