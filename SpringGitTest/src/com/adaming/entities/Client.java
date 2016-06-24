8aab9931a234411a9f5c286728e1e7b49ed98e35
inti0298 <inti0298@DESKTOP-66N0E79>
2016-06-24 14:03:35 +0200

    c

--- SpringGitTest/src/com/adaming/entities/Client.java
+++ SpringGitTest/src/com/adaming/entities/Client.java
@@ -6,5 +6,24 @@ public class Client {
 	
 	 private Long idClient;
 	 private Date dateDeNaissance;	// Date
+	 private Long x;
+	public Long getIdClient() {
+		return idClient;
+	}
+	public void setIdClient(Long idClient) {
+		this.idClient = idClient;
+	}
+	public Date getDateDeNaissance() {
+		return dateDeNaissance;
+	}
+	public void setDateDeNaissance(Date dateDeNaissance) {
+		this.dateDeNaissance = dateDeNaissance;
+	}
+	public Long getX() {
+		return x;
+	}
+	public void setX(Long x) {
+		this.x = x;
+	}
 
 }
