28a21dc0ca0d21587189cb33e519b3fd92c8d286
inti0298 <inti0298@DESKTOP-66N0E79>
2016-06-24 14:10:41 +0200

    c

--- SpringGitTest/src/com/adaming/entities/Driver.java
+++ SpringGitTest/src/com/adaming/entities/Driver.java
@@ -5,7 +5,10 @@ public class Driver {
 	public static void main(String[] args) {
 		
 		
-		System.out.println(");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
 
 	}
 
--- SpringGitTest/src/com/adaming/entities/Personne.java
+++ SpringGitTest/src/com/adaming/entities/Personne.java
@@ -3,4 +3,9 @@ package com.adaming.entities;
 public class Personne {
 	private Long idPersonne;
 	private String nom;
+	public Personne(Long idPersonne, String nom) {
+		super();
+		this.idPersonne = idPersonne;
+		this.nom = nom;
+	}
 }
