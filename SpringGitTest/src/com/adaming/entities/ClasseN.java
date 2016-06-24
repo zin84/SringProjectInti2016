6906c6204b68145b024981966b82edf8d4f1d6c2
inti0299 <inti0299@DESKTOP-NIFRS8F>
2016-06-24 13:00:23 +0200

    sdgf

--- SpringGitTest/src/com/adaming/entities/ClasseN.java
+++ SpringGitTest/src/com/adaming/entities/ClasseN.java
@@ -4,7 +4,7 @@ public class ClasseN {
 	private Long idClaseN;
 	private void sysout() {
 		// TODO Auto-generated method stub
-
+sysout();
 	}
 	private void sysout01() {
 		// TODO Auto-generated method stub
